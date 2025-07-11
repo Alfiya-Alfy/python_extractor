05aa0ba3316fa79cc569c198b2d3257cb8e7a651
Alfiya Nazer <alfiyanazer008@gmail.com>
2025-07-11 09:58:03 +0530

    Modified the extract.py file

--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 from pypdf import PdfReader
 reader_obj=PdfReader('new.pdf')
 print(len(reader_obj.pages))
-page=reader_obj.pages[0]
-text=page.extract_text()
+page_pdf=reader_obj.pages[0]
+text=page_pdf.extract_text()
 print(text)
